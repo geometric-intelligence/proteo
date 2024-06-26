76c3c8b21c0d9ae19d7d11a705b9c0d2b62deb95
cornelislouisa <cornelislouisa@gmail.com>
2024-06-26 15:12:30 -0700

    things are running. Cuda memory error

--- proteo/main.py
+++ proteo/main.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pytorch_lightning as pl
 from ray import tune
 from ray.air.integrations.wandb import WandbLoggerCallback, setup_wandb
@@ -27,14 +28,14 @@ def train_func(search_config):
     """
     config = read_config_from_file(CONFIG_FILE)
     #TODO: Hacky - is there a better way?
-    model_name = search_config['train_loop_config']['model']
-    search_config['model'] = model_name
-    updated_parameters = {'hidden_channels': search_config['train_loop_config']['hidden_channels'], 
-                         'heads':search_config['train_loop_config']['heads']}
+    # Update model specific parameters
+    model_name = search_config['model']
+    updated_parameters = {'hidden_channels': search_config['hidden_channels'], 
+                         'heads':search_config['heads'], 'lr': search_config['lr']}
     config[model_name].update(updated_parameters)
-    search_config.pop('train_loop_config')
-    print("=====================================")
-    print(search_config)
+    # Remove keys that were already updated in nested configuration
+    for key in updated_parameters:
+        search_config.pop(key)
     config.update(search_config)
     setup_wandb(search_config, api_key_file = config.wandb_api_key_path)
     
@@ -43,8 +44,7 @@ def train_func(search_config):
     train_loader, test_loader = proteo_train.construct_loaders(config, train_dataset, test_dataset)
 
     in_channels = train_dataset.feature_dim  # 1 dim of input
-    out_channels = train_dataset.label_dim  # 1 dim of result
-
+    out_channels = train_dataset.label_dim  # 1 dim of resul÷≥…t
     avg_node_degree = proteo_train.avg_node_degree(test_dataset)
 
     module = proteo_train.Proteo(config, in_channels, out_channels, avg_node_degree)
@@ -87,33 +87,43 @@ def search_hyperparameters():
         run_config=run_config,
     )
 
-    hidden_channels_map = {'gat': [3, 10, 20, 25], 'gat-v4': [[8, 8, 12], [10, 7, 12], [8, 16, 12]]}
-    heads_map = {'gat': [1, 3, 5], 'gat-v4': [[2, 2, 3], [3, 4, 5], [4, 4, 6]]}
-
-    def generate_configurations():
-        configurations = []
-        for model in ['gat', 'gat-v4']:
-            for hidden_channels in hidden_channels_map[model]:
-                for heads in heads_map[model]:
-                    configurations.append({
-                        'model': model,
-                        'hidden_channels': hidden_channels,
-                        'heads': heads
-                    })
-        return configurations
+    def hidden_channels(spec):
+        # Note that hidden channels must be divisible by heads for gat
+        hidden_channels_map = {'gat': [4, 8, 12, 20] , 'gat-v4': [[8, 8, 12], [10, 7, 12], [8, 16, 12]]}
+        config = spec['train_loop_config']
+        model_params = hidden_channels_map[config['model']]
+        random_index = np.random.choice(len(model_params))
+        return model_params[random_index]
+    
+    def heads(spec):
+        heads_map = {'gat': [1, 2, 4], 'gat-v4': [[2, 2, 3], [3, 4, 5], [4, 4, 6]]}
+        config = spec['train_loop_config'] 
+        model_params = heads_map[config['model']]
+        random_index = np.random.choice(len(model_params))
+        return model_params[random_index]
 
     search_space = {
-        'train_loop_config': tune.grid_search(generate_configurations()),
+        'model': tune.grid_search(['gat', 'gat-v4']),
         'seed': tune.randint(0, MAX_SEED),
+        'hidden_channels': tune.sample_from(hidden_channels),
+        'heads': tune.sample_from(heads),
         'lr': tune.loguniform(1e-4, 1e-1),
+        'batch_size': tune.choice([6, 12, 32, 64, 80]),
 }
 
     def trial_str_creator(trial):
         config = trial.config['train_loop_config']
         seed = config['seed']
-        nested_config = config['train_loop_config']
-        return f"model={nested_config['model']},heads={nested_config['heads']},seed={seed}"
+        return f"model={config['model']},heads={config['heads']},seed={seed}"
 
+    config = read_config_from_file(CONFIG_FILE)
+    scheduler = ASHAScheduler(
+        time_attr="training_iteration",
+        max_t= config['epochs'],
+        grace_period=2,
+        reduction_factor=4,
+    )
+    
     tuner = tune.Tuner(
         ray_trainer,
         param_space={'train_loop_config': search_space},
@@ -121,8 +131,9 @@ def search_hyperparameters():
             metric='val_loss',
             mode='min',
             # TODO: Infer num samples from search space.
-            num_samples=2, # Repeats grid search options 2 times through with random seed
+            num_samples=30, # Repeats grid search options 2 times through with random seed
             trial_name_creator=trial_str_creator,
+            scheduler=scheduler,
         ),
     )
     return tuner.fit()
